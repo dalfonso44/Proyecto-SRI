a8130be392578858d96268278f9183b2fed17fa0
Diamis Alfonso <91924607+dalfonso44@users.noreply.github.com>
2022-12-18 19:40:31 -0500

    boolean_ok

---
+++ modelos/booleano.py
@@ -0,0 +1,136 @@
+from ..nlp.languaje_procesing import normalize_text
+import os
+from collections import defaultdict
+import glob
+import numpy as np
+
+class BooleanModel(object):
+    def __init__(self, Corpus) -> None:
+
+        #path to the documents
+        self.Corpus = Corpus 
+
+        # matrix for indexed docs
+        self.inverted_matrix = defaultdict(list)
+
+        #
+        self.terms = []
+
+        #
+        self.documents = dict()
+
+        def preprocesing_corpus(self):
+            index = 1
+            for filename in glob.glob(self.Corpus):
+                with open(filename,"r") as file:
+                    text = file.read()
+                normalized_text = normalize_text(text)  
+
+                # erase repeted words
+                normalized_text = list(set(normalized_text))  
+
+                # create inverted matrix for index terms
+                for word in normalized_text:
+                    self.inverted_matrix[word].append(index) 
+                    self.terms.append(word)
+                self.documents[index] = (os.path.basename(filename),"resumen") 
+                index += 1  
+
+        preprocesing_corpus()    
+
+
+
+    # E -> BX
+    # X -> OR BX | epsilon
+    # B -> AND CY 
+    # Y -> AND CY |epsilon
+    # C -> D | not D 
+    # D -> term | (A)         
+
+
+    def proces_query(self,query):
+        query_tokens = self.lexer(query)
+        vector = self._evaluate_query(query_tokens)
+        relevant_docs = dict()
+        for i in range (len(self.documents)):
+            if vector[i] == True:
+                yield self.documents[i+1]        
+
+
+    def lexer(self,query):
+        tokens = [] 
+        for item in query.split():
+            tokens.append(item)
+        return tokens    
+
+
+    def _evaluate_query(self,query_tokens):
+        i, vector= self._parse_expression(query_tokens,0)
+        if i!=len(query_tokens):
+            print("La expresion no es correcta")
+            return np.zeros(len(self.documents), dtype=bool)
+        return vector   
+
+    def _parse_expression(self,tokens,i):
+        i,term = self._parse_B(tokens,i) 
+        return self._parse_X(tokens,i,term)
+
+    def _parse_X(self,tokens,i,value):
+        if i < len(tokens):
+            if tokens[i] == 'OR':
+                i,term2 = self._parse_B(tokens,i+1)
+                value = value | term2
+                return self._parse_X(tokens,i,value)
+        return i,value        
+
+    def _parse_B(self,tokens,i):
+        i,factor = self._parse_C(tokens,i)
+        return self._parse_Y(tokens,i,factor)
+
+    def _parse_Y(self,tokens,i,value):
+        if i<len(tokens):
+            if tokens[i] == 'AND':
+                i,factor2 = self._parse_C(tokens,i+1)
+                value = value & factor2
+                return self._parse_Y(tokens,i,value)
+        return i,value        
+
+    def _parse_C(self,tokens,i) :
+        if i < len(tokens):
+            if tokens [i] == 'not':
+                i, vector = self._parse_C(tokens,i+1)            
+                return i, ~vector
+            elif self._is_term(tokens[i]):
+                vector = self._vector(tokens[i])
+                return i + 1, vector
+            elif tokens[i] == '(':
+                i, vector = self._parse_expression(tokens,i+1) 
+                if tokens[i] != ')':
+                  print ("Expresion mal formada")  
+                return i + 1,vector   
+            else:
+                print("Expresion mal formada")  
+                return -1,np.zeros(len(self.documents),dtype = bool)
+        else:
+            print("Expresion mal formada")
+            return -1,np.zeros(len(self.documents),dtype = bool)            
+
+        
+
+    def _is_term(self,token):
+        if token == 'AND' or token == 'OR' or token == '(' or token ==')' or token == 'not':
+            return False
+        return True         
+
+    def _vector(self,term):
+        docs_count = len(self.documents)
+        if term in self.Terms:
+            vector = np.zeros(docs_count,dtype=bool)
+
+            postings = self.inverted_matix[term]
+            for doc_id in postings:
+                vector[doc_id-1] = True
+            return vector
+        else:
+            print("The term"+ term + " Was not found in the corpus")                
+            return np.zeros(docs_count,dtype=bool)    
---
+++ modelos/vectorial.py
@@ -0,0 +1,159 @@
+from fileinput import filename
+import glob
+import math
+from operator import invert
+import os
+from collections import defaultdict
+from ..nlp.languaje_procesing import normalize_text
+from functools import reduce
+
+class VectorSpaceModel(object):
+    def __init__(self, Corpus) -> None:
+
+        self.Corpus = Corpus
+
+
+        self.documents = dict()
+
+
+        self.documents_vector = defaultdict(list)
+
+        self.query_vector = list()
+
+        
+        self.vocabulary = set()
+
+        # ahora se supone que cuando no tenga nada ya tiene un cero
+        self.postings = defaultdict([0 for i in range(glob.glob(self.Corpus))])
+
+        self.query_postings = defaultdict(int)
+
+
+        self.global_terms_frequency = defaultdict(int)
+
+        
+        self.documents_norm = defaultdict(float)
+
+        def preprocesing_corpus(self):
+            index = 1
+            for filename in glob.glob(self.Corpus):
+                with open(filename,"r") as file:
+                    text = file.read()
+                normalized_text = normalize_text(text) 
+
+                unique_terms = set(normalized_text)
+
+                self.documents_vector[index] = unique_terms
+                self.vocabulary = self.vocabulary.union(unique_terms)
+
+
+                for term in unique_terms:
+                    self.postings[term][index] = text.count(term) # frecuencia del termino en el doc
+                    self.global_terms_frequency[term] += 1
+
+                self.documents[index] = os.path.basename(filename)
+                index += 1
+
+        preprocesing_corpus()
+
+
+    def proces_query(self,query):
+        self.query_vector = self.lexer(query)
+        scores = defaultdict(float)
+        for id in range(1,len(self.documents)):
+            scores[id] = self.similarity(query,query_postings,id)
+
+        return scores
+
+
+    def similarity(self, doc):
+        similarity = 0.0
+        query_norm = 0
+
+        for term in self.query_vector:
+            weight_in_query = self.weight_in_query(term)
+            query_norm += weigth_in_query**2
+            weigth_in_document = self.weight_in_document(term, doc_id)
+            similarity += (weigth_for_term_in_query * weigth_for_term_in_document) # Dj * Q
+
+        query_norm = math.sqrt(query_norm)
+        doc_norm = self.documents_norm[doc]
+        similarity = similarity / (query_norm * doc_norm)   
+        return similarity 
+
+
+
+
+
+    def weight_in_document(self, term,doc):
+        tf_ij = self.normalized_term_frequency(term,doc)
+        idf_i = self.inverse_document_frequecy(term)
+        weight = tf_ij * idf_i
+        return weight
+
+    def weight_in_query(self,term):
+        alpha = 0.4 # termino de suavizado
+        tf_iq = self.normalized_term_frequency(term,-1)
+        idf_i = self.inverse_document_frequecy(term)
+
+        weight = (alpha + (1+alpha)*tf_iq) * idf_i
+        return weight
+
+
+
+
+    def normalized_term_frequency(self,term, doc): # si doc = -1 -> el documento es la consulta
+        frequency = 0
+        if doc == -1 :   
+            frequency = self.query_postings[term] # el documento es la consulta
+        else :
+            frequency = self.postings[term][doc]    
+
+        if frequency == 0:
+            return 0
+        max_frequency = 1 # no es cero pa que no de mal la division
+
+        if doc == -1:
+            for term in self.query_vector:
+                freq = self.query_postings[term]
+                if max_frequency < freq:
+                    max_frequency = freq
+        else :
+            for terms in self.documents_vector[doc]:
+                freq = self.postings[term][doc]
+                if max_frequency < freq:
+                    max_frequency = freq    
+        
+        normalized_frequency = frequency / max_frequency # ver que pasa si esto es cero
+        return normalized_frequency
+
+    def inverse_document_frequecy(self,term):
+        N = len(self.documents)    
+        n_i = self.global_terms_frequency[term]     
+
+        #revisar esto, sumar 1 arriba y abajo pa que nunca de cero
+        idf = math.log((N/ni),2)   
+        return idf
+
+
+
+    def lexer(self,query):
+        normalized_query = normalize_text(query)
+        for term in normalized_query:
+            self.query_postings[term] = normalized_query.count(term)
+
+        normalized_query = list(set(normalized_query))    
+
+        return normalized_query
+
+
+
+
+
+
+            
+
+
+
+
+        
